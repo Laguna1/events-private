ecc43695c60cca1364bb37c70a99671944e7a993
Laguna1 <lagunazoomarket@gmail.com>
2021-10-04 01:29:03 +0300

    Delete User model & sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'users#show'
-  get '/signup', to: 'users#new'
-  get '/signin', to: 'sessions/new'
-  get '/signin', to: 'sessions/create'
- 
-  resources :users, only: [:new, :show, :create]
-  resources :users, only: [:new, :destroy, :create]
   
-  # root 'events#index'
+  root 'events#index'
   resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_03_212303) do
+ActiveRecord::Schema.define(version: 2021_10_03_204717) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.string "place"
     t.datetime "date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
