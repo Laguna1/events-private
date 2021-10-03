7102e3367794aa98a324c92cd3b808beb6bc1a4a
Laguna1 <lagunazoomarket@gmail.com>
2021-10-04 00:40:55 +0300

    Add create user action, update root path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,19 @@ class UsersController < ApplicationController
    @user = User.new
   end
 
-  def create; end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
 
   def show; end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'users#show'
   get '/signup', to: 'users#new'
   resources :users, only: [:new, :index, :create]
-  root 'events#index'
+  
+  # root 'events#index'
   resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
