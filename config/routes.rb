57ffa426ac67b059715b6632a6c20a929f0b014f
Laguna1 <lagunazoomarket@gmail.com>
2021-10-04 00:29:18 +0300

    Add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'users/show'
   root 'events#index'
   resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
