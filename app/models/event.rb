da1925a21f0e19a8d36b03624215f5495d485f05
Laguna1 <lagunazoomarket@gmail.com>
2021-10-05 21:54:14 +0300

    Define scopes for past- & upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :visitings, dependent: :destroy
   has_many :visitors, through: :visitings, source: :user
+
+  scope :past_events, -> { where('date < ?', Date.today) }
+  scope :upcoming_events, -> { where('date >= ?', Date.today) }
 end
